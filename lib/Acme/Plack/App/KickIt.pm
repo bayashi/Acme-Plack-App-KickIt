9602a345eebd501d82d91cb334b028b1e32c1cd6
bayashi <bayashi@cpan.org>
2021-01-19 13:01:08 +0000

    Viva #kichijojipm

--- lib/Acme/Plack/App/KickIt.pm
+++ lib/Acme/Plack/App/KickIt.pm
@@ -2,14 +2,129 @@ package Acme::Plack::App::KickIt;
 use strict;
 use warnings;
 use Carp qw/croak/;
+use parent qw/Plack::Component/;
+use Plack::Request;
+use Plack::Util::Accessor qw/
+    repos_dir
+    secret
+/;
+use JSON qw/decode_json/;
 
 our $VERSION = '0.01';
 
-sub new {
-    my $class = shift;
-    my $args  = shift || +{};
+sub prepare_app {
+    my $self = shift;
 
-    bless $args, $class;
+    if (!defined $self->repos_dir) {
+        croak 'required `repos_dir`.';
+    }
+
+    $self->repos_dir($self->_normalize_repos_dir($self->repos_dir));
+
+    $self->secret(defined $self->secret ? $self->secret : '');
+
+    return $self;
+}
+
+sub _normalize_repos_dir {
+    my ($self, $repos_dir) = @_;
+
+    $repos_dir =~ s/^~/$ENV{HOME}/;
+
+    if (!-e $repos_dir) {
+        croak sprintf('repos_dir:%s does not exists.', $repos_dir);
+    }
+
+    return $repos_dir;
+}
+
+sub call {
+    my ($self, $env) = @_;
+
+    my $req  = Plack::Request->new($env);
+
+    if ($req->method eq 'POST') {
+        return $self->_kick_it($req);
+    }
+
+    return $self->_call_content($req);
+}
+
+sub _call_content {
+    my ($self, $req) = @_;
+
+    if (($req->parameters->{secret} || '') ne $self->secret) {
+        croak 'Forbidden.';
+    }
+
+    my $secret = $req->parameters->{secret} || '';
+
+    my $content = <<_HTML_;
+<html>
+<head></head>
+<body>
+<div id="result"></div>
+<script>
+let data = new Object();
+data.path = document.location.pathname;
+data.line = document.location.hash.match(/^#L(\\d+)/);
+data.line = data.line[1];
+data.secret = '$secret';
+let request = new XMLHttpRequest();
+request.open('POST', '/');
+request.onload = function() {
+  window.close();
+};
+request.send(JSON.stringify(data));
+</script>
+</body>
+</html>
+_HTML_
+
+    return [200, ['Content-Type' => 'text/html'], [$content]];
+}
+
+sub _kick_it {
+    my ($self, $req) = @_;
+
+    my $params = decode_json($req->content);
+
+    if (($params->{secret} || '') ne $self->secret) {
+        croak 'Forbidden.';
+    }
+
+    my $command = $self->_build_command($params);
+
+    system $command;
+    warn "$command\n";
+
+    return [200, [], ["Kicked '$command'"]];
+}
+
+sub _build_command {
+    my ($self, $params) = @_;
+
+    my $repos_dir = $params->{repos_dir} ? $self->_normalize_repos_dir($params->{repos_dir}) : $self->repos_dir;
+
+    my $local_lib_path = $repos_dir . '/' . $self->_get_local_lib_path($params->{path});
+
+    if (!-f $local_lib_path) {
+        croak sprintf('File:%s Not Found', $local_lib_path);
+    }
+
+    my $line = $params->{line} || 1;
+
+    my $command = 'code -g ' . $local_lib_path . ':' . $line;
+
+    return $command;
+}
+
+sub _get_local_lib_path {
+    my ($self, $path) = @_;
+
+    my (undef, $account_name, $repos_name, $blob, $branch, @paths) = split '/', $path;
+
+    return join '/', ($repos_name, @paths);
 }
 
 1;
@@ -20,12 +135,12 @@ __END__
 
 =head1 NAME
 
-Acme::Plack::App::KickIt - one line description
+Acme::Plack::App::KickIt - Kick your ed
 
 
 =head1 SYNOPSIS
 
-    use Acme::Plack::App::KickIt;
+    plackup -MAcme::Plack::App::KickIt -e 'Acme::Plack::App::KickIt->new(repos_dir=>"~/repos_dir")->to_app'
 
 
 =head1 DESCRIPTION
